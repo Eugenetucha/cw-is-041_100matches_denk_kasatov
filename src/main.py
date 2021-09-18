bc686df70833ff56e29b70644ec78dd71b11e873
oriz041 <aleks.kasatik@gmail.com>
2021-09-18 18:55:18 +0700

    Выполнена 1 задача

---
+++ src/main.py
@@ -0,0 +1,52 @@
+import pygame
+import os
+import game_starting
+
+
+gameScreen = pygame.display.set_mode((400, 300))
+button1 = pygame.Rect(20, 20, 460, 70)
+button2 = pygame.Rect(20, 100, 460, 70)
+
+
+def early_dysplay():
+    list_crnt()
+    global button1
+    global button2
+    global gameScreen
+    pygame.init()
+    pygame.font.init()
+    x = 100
+    y = 100
+    os.environ['Sp_VIDEO_WINDOW_POS'] = "%d,%d" % (x, y)
+    size = [500, 500]
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("100 matches")
+    gameScreen.fill((255, 255, 255))
+    pygame.draw.rect(screen, (0, 200, 64), button1)
+    pygame.draw.rect(screen, (0, 200, 64), button2)
+    myfont = pygame.font.SysFont('Comic Sans MS', 30)
+    textsurface1 = myfont.render('Играть', False, (0, 0, 0))
+    screen.blit(textsurface1, (200, 50))
+    textsurface2 = myfont.render('Выйти', False, (0, 0, 0))
+    screen.blit(textsurface2, (200, 130))
+    textsurface3 = myfont.render('Лидерборд', False, (0, 0, 0))
+    screen.blit(textsurface3, (200, 210))
+    textsurface4 = myfont.render('Player 1', False, (0, 0, 0))
+    screen.blit(textsurface4, (150, 230))
+    textsurface5 = myfont.render('Player 2', False, (0, 0, 0))
+    screen.blit(textsurface5, (280, 230))
+    textsurface4 = myfont.render(str(crnt_player1), False, (0, 0, 0))
+    screen.blit(textsurface4, (150, 260))
+    textsurface5 = myfont.render(str(crnt_player2), False, (0, 0, 0))
+    screen.blit(textsurface5, (280, 260))
+    pygame.display.flip()
+    pressed()
+    runGame = True
+    while runGame:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                runGame = False
+    pygame.quit()
+
+if __name__ == '__main__':
+    early_dysplay()
